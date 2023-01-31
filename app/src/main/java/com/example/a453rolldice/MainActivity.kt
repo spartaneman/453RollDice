d90539972fa619a9e31ccf91350d817608941c9f
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-30 16:47:24 -0800

    Finished Code Lab 2

--- app/src/main/java/com/example/a453rolldice/MainActivity.kt
+++ app/src/main/java/com/example/a453rolldice/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.a453rolldice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a453rolldice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rollButton.setOnClickListener {
+            rollDice()
+        }
+
+        binding.countUpButton.setOnClickListener {
+            countUp()
+        }
+
+    }
+
+    private fun rollDice(){
+        val randomInt = (1..6).random()
+        binding.diceOutcomeTextview.text = randomInt.toString()
+    }
+    private fun countUp(){
+        val textInt =binding.diceOutcomeTextview.text.toString().toInt()
+
+        if(textInt == 6)
+        {
+            binding.diceOutcomeTextview.text = textInt.toString()
+        }
+        else
+        {
+            binding.diceOutcomeTextview.text = "${textInt + 1}"
+        }
+
     }
 }
